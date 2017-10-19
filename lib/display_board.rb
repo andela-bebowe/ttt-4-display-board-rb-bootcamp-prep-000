943a06f3be02ebaa1eec6b4beaf74d1519e185da
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-19 12:11:32 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,9 +10,11 @@ def display_board(board)
   str = ""
   board.each_with_index do |val, index|
     is_third_row = (index + 1) % 3 == 0
-    is_ninth_value = (index + 1) == 9
+    is_last_value = (index + 1) == board.length
     if (is_third_row & !is_ninth_value)
       str += " #{val} \n#{divider}"
+    elsif is_last_value
+      str += " #{val} "
     else
       str += " #{val} |"
     end
