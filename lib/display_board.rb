aa0d7f92ca83c9318a6be1c22d4163e67f20488b
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-19 12:04:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,8 +7,8 @@ def box
 end
 
 def display_board(board)
-  board.each_with_index do |val, index|
-    is_third_value = (index + 1) % 3 == 0
+  board.each_with_index do |val, ind|
+    is_third_value = (ind + 1) % 3 == 0
     if (is_third_value)
       str += " #{val} \n\n#{divider}"
     else
